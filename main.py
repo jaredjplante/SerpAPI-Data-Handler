6bdd8358d96d717ca9718cb0308c9e7d27bea241
jaredjplante <jaredjplante@gmail.com>
2024-02-05 19:36:39 -0500

    added format_data and write_to_database

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def setup_db(cursor: sqlite3.Cursor):
     title TEXT NOT NULL,
     company TEXT NOT NULL,
     location TEXT NOT NULL,
-    salary TEXT NOT NULL,
-    remote TEXT NOT NULL,
+    salary TEXT NOT NULL DEFAULT 'Not Specified',
+    remote TEXT NOT NULL DEFAULT 'Not Specified',
     age TEXT NOT NULL,
     description TEXT NOT NULL
     );''')
@@ -55,17 +55,57 @@ def get_data():  # add comment to test workflow
     return results
 
 
-def write_data():
-    with open('JobData.txt', 'a', encoding='utf-8') as f:
-        while params["start"] < 41:
-            results = get_data()
-            for key, value in results.items():
-                if key == 'jobs_results':
-                    for item in value:
-                        for key2 in item.keys():
-                            f.write(f"{key2} : {item.get(key2)}\n")
-                        f.write("\n")
-            params["start"] += 10
+# def write_data():
+#     with open('JobData.txt', 'a', encoding='utf-8') as f:
+#         while params["start"] < 41:
+#             results = get_data()
+#             for key, value in results.items():
+#                 if key == 'jobs_results':
+#                     for item in value:
+#                         for key2 in item.keys():
+#                             f.write(f"{key2} : {item.get(key2)}\n")
+#                         f.write("\n")
+#             params["start"] += 10
+
+
+def format_data(cursor: sqlite3.Cursor):
+    while params["start"] < 41:
+        results = get_data()
+        for key, value in results.items():
+            if key == 'jobs_results':
+                for item in value:
+                    title = 'na'
+                    company = 'na'
+                    location = 'na'
+                    age = 'na'
+                    description = 'na'
+                    salary = 'Not Specified'
+                    remote = 'Not Specified'
+                    qualifications = 'na'
+                    for key2 in item.keys():
+                        #     f.write(f"{key2} : {item.get(key2)}\n")
+                        # f.write("\n")
+                        if key2 == 'title':
+                            title = item.get(key2)
+                        elif key2 == 'company_name':
+                            company = item.get(key2)
+                        elif key2 == 'location':
+                            location = item.get(key2)
+                        elif key2 == 'description':
+                            description = item.get(key2)
+                        elif key2 == 'extensions':
+                            age = item.get(key2)[1]
+                            if item.get(key2)[2] == 'Work from home':
+                                remote = 'yes'
+                        elif key2 == 'job_highlights':
+                            qualifications = item.get(key2)[1]
+                    write_to_database(title, company, location, age, description, salary, remote, qualifications)
+
+        params["start"] += 10
+
+
+def write_to_database(title, company, location, age, description, salary, remote, qualifications):
+    print("hello")
 
 
 def main():
