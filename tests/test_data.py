541cdaf793aef5b59064a9e138de6dda5f8750d0
jaredjplante <jaredjplante@gmail.com>
2024-02-20 20:07:26 -0500

    added test_check_old_tables to make sure old tables still exist

--- tests/test_data.py
+++ tests/test_data.py
@@ -52,7 +52,6 @@ def test_check_excel_table():
     # test to see if excel data matches table
     # assumes data was already written to table
     conn, cursor = databaseutils.open_db("JobData.sqlite")
-    databaseutils.setup_db(cursor)
     data = excelutils.get_excel_data("Sprint3Data.xlsx")
     cursor.execute("SELECT company_name, posted_at, job_title FROM listings_excel WHERE job_id = '2907e6de3fa805a1'")
     record = cursor.fetchone()
@@ -69,5 +68,17 @@ def test_check_excel_table():
     assert data[2]['company_name'] == record[0]
     assert data[2]['posting_age'] == record[1]
     assert data[2]['job_title'] == record[2]
+
+    databaseutils.close_db(conn)
+
+
+def test_check_old_tables():
+    conn, cursor = databaseutils.open_db("JobData.sqlite")
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='listings'")
+    table_bool = cursor.fetchone()
+    assert table_bool
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='qualifications'")
+    table_bool = cursor.fetchone()
+    assert table_bool
 
     databaseutils.close_db(conn)
